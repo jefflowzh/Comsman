fde593a73a31e093ce765d6723b0c083b9c577e0
weidonglim <e0310944@comp.nus.edu.sg>
2020-03-30 14:55:59 +0800

    fixed create customer

--- ComputerPartsEcommerce-ejb/src/java/entity/Customer.java
+++ ComputerPartsEcommerce-ejb/src/java/entity/Customer.java
@@ -33,6 +33,9 @@ public class Customer extends User implements Serializable {
     private List<CustomerOrder> orders;
 
     public Customer() {
+        cart = new ArrayList<>();
+        loyaltyPoints = 0;
+        orders = new ArrayList<>();
     }
     
     public Customer(String cardNumber, String ccv, String firstName, String lastName, String address, String email, String password, String contactNumber) {
