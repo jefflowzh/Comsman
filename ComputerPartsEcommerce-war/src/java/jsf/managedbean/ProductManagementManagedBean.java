b95a3badbc51f4ff17fe0294b141c1501da48ec2
Bryan Soh <bryansoh95@gmail.com>
2020-04-29 22:45:16 +0800

    enabled ProductManagementManagedBean reset

--- ComputerPartsEcommerce-war/src/java/jsf/managedbean/ProductManagementManagedBean.java
+++ ComputerPartsEcommerce-war/src/java/jsf/managedbean/ProductManagementManagedBean.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package jsf.managedbean;
 
 import datamodel.StringValue;
@@ -325,6 +320,7 @@ public class ProductManagementManagedBean implements Serializable {
     }
 
     public void doCreateNewProduct(ActionEvent event) {
+        FacesContext.getCurrentInstance().getViewRoot().getViewMap().clear();
         if (selectedProduct.equals("CPU")) {
             newProduct = new CPU();
         } else if (selectedProduct.equals("MotherBoard")) {
